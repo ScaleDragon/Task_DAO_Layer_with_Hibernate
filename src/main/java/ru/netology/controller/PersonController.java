7ef2b0bfb6eb41ba7543a7d441cfff6ba035c4e8
Vyacheslav L. <Dwek18@mail.ru>
2024-11-23 23:25:45 +0300

    update Person and name classes

--- src/main/java/ru/netology/controller/PersonController.java
+++ src/main/java/ru/netology/controller/PersonController.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.person.Person;
-import ru.netology.repository.Repository;
+import ru.netology.repository.PersonRepository;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
-public class Controller {
-    private final Repository repository;
+public class PersonController {
+    private final PersonRepository repository;
 
     @GetMapping("/persons/by-city")
     protected List<Person> getPersonsByCity(@RequestParam(value = "city", required = false) String city) {
--- src/main/java/ru/netology/person/Person.java
+++ src/main/java/ru/netology/person/Person.java
@@ -1,7 +1,6 @@
 package ru.netology.person;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,50 +14,14 @@ import lombok.NoArgsConstructor;
 @Table(name = "PERSONS")
 public class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(length = 20, nullable = false)
-    private String name;
-
-    @Column(length = 20, nullable = false)
-    private String surname;
-
-    @Min(0)
-    @Column()
-    private int age;
+    @EmbeddedId
+    private PersonId personId;
 
     @Column(length = 20, nullable = false, name = "phone_number")
     private String phoneNumber;
 
     @Column(nullable = false, name = "city_of_living", length = 30)
     private String city;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Min(0)
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(@Min(0) int age) {
-        this.age = age;
-    }
 
     public String getPhoneNumber() {
         return phoneNumber;
